fe470c316b9c25ce5f066198f45b20956155a8f7
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2025-04-29 18:23:29 +0300

    кодинг на созвоне

--- create-tables.sql
+++ create-tables.sql
@@ -1,8 +1,15 @@
 CREATE DATABASE calendar_builder;
 
 CREATE TABLE IF NOT EXISTS events (
-    id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    base_type VARCHAR(50) NOT NULL
+    -- id SERIAL PRIMARY KEY,
+    date DATE NOT NULL PRIMARY KEY,
+    stars INT NULL DEFAULT NULL,
+    base_types VARCHAR(50) NOT NULL,
+    holiday VARCHAR NULL DEFAULT NULL,
+    
 );
+
+
+id  date        stars  base_types holiday
+1   2025-04-29  3      "tl,a"        Лосар
 
--- event_storage_postgresql.py
+++ event_storage_postgresql.py
@@ -2,11 +2,18 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from config_reader import env_config
 from entity.event import Event
-from typing import List, Optional
+from typing import List, Optional, NamedTuple
 import calendar
 from datetime import date
 
 from sqlalchemy import text
+
+
+class DayEvents(NamedTuple):
+    stars: int
+    day_class: str | None
+    is_amrita: bool
+    moon: str | None
 
 
 class EventStorageSqlAlchemy:
@@ -14,7 +21,7 @@ class EventStorageSqlAlchemy:
         connection_str = f"postgresql+pg8000://{env_config.postgresql_username}:{env_config.postgresql_password}@{env_config.postgresql_hostname}:{env_config.postgresql_port}/{env_config.postgresql_database}"
         self.engine = create_engine(connection_str, echo=True)
 
-    def get_events_calendar_view(self, year: int, month: int):
+    def get_events_calendar_view(self, year: int, month: int) -> List[DayEvents]:
         _, month_days = calendar.monthrange(year, month)
         start_date = date(year, month, 1)
         end_date = date(year, month, month_days)
@@ -35,7 +42,33 @@ class EventStorageSqlAlchemy:
             result = session.execute(
                 query, {"start_date": start_date, "end_date": end_date}
             ).fetchall()
-            events_by_day = [[] for i in range(month_days)]
+            events_by_day = [[] for _ in range(month_days)]
+
+            def get_day_events(events: list) -> DayEvents:
+                stars = 0
+                day_class = None
+                is_amrita = False
+                moon = None
+
+                for event in events:
+                    if "stars_" in event:
+                        stars = int(event[-1])
+                    if event in ['bm', 'gr', 'bsh', 'dk']:
+                        day_class = event
+                    if "tl" == event:
+                        moon = "🌑"
+                    if "ba" == event:
+                        moon = "🌕"
+                    if "a" == event:
+                        is_amrita = True
+
+                return DayEvents(
+                    stars=stars,
+                    day_class=day_class,
+                    is_amrita=is_amrita,
+                    moon=moon,
+                )
+
             for day, event_types in result:
-                events_by_day[day - 1] = event_types
+                events_by_day[day - 1] = get_day_events(event_types)
             return events_by_day
